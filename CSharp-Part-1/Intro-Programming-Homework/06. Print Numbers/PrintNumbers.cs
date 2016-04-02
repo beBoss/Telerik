e8fcc01857b4acbb4cda3ec8de83eb63e137f3d1
beBoss™ <beBoss.bg@gmail.com>
2016-04-02 17:08:19 +0100

    Added Problem 06. Print Numbers Added Problem 07. First and Last Name

---
+++ CSharp-Part-1/Intro-Programming-Homework/06. Print Numbers/PrintNumbers.cs
@@ -0,0 +1,12 @@
+﻿namespace _06.Print_Numbers
+{
+    using System;
+
+    public class PrintNumbers
+    {
+        public static void Main()
+        {
+            Console.WriteLine("1\r\n101\r\n1001");
+        }
+    }
+}
---
+++ CSharp-Part-1/Intro-Programming-Homework/07. First and Last Name/PrintFirstLastName.cs
@@ -0,0 +1,12 @@
+﻿namespace _07.First_and_Last_Name
+{
+    using System;
+
+    public class PrintFirstLastName
+    {
+        public static void Main()
+        {
+            Console.WriteLine("beBoss\r\nThe Boss");
+        }
+    }
+}
