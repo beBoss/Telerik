1f4e62c2f69ea6e66691525992083c99c06f4df6
beBoss™ <beBoss.bg@gmail.com>
2016-04-02 16:48:38 +0100

    Added CSharp-Part-1 folder Added CSharp-Part-1/.gitignore Added Topic Intro-Programming-Homework Added Solution Intro-Programming-Homework.sln Added 04. HelloWorld problem

---
+++ CSharp-Part-1/Intro-Programming-Homework/04. Hello World/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿namespace _04.Hello_World
+{
+    using System;
+
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello, C#!");
+        }
+    }
+}
