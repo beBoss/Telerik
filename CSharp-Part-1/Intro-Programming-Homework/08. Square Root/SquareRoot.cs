4c3b6a816493bf161250f95658a3438440330bcd
beBoss™ <beBoss.bg@gmail.com>
2016-04-02 17:13:55 +0100

    Added problem 08. Square Root

---
+++ CSharp-Part-1/Intro-Programming-Homework/08. Square Root/SquareRoot.cs
@@ -0,0 +1,12 @@
+﻿namespace _08.Square_Root
+{
+    using System;
+
+    public class SquareRoot
+    {
+        public static void Main()
+        {
+            Console.WriteLine(Math.Sqrt(12345));
+        }
+    }
+}
