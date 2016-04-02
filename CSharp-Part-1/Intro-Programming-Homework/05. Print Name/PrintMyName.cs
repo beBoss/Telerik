2f199432c21d30be3f1cbbff88da02d7e3011898
beBoss™ <beBoss.bg@gmail.com>
2016-04-02 16:59:44 +0100

    Added problem 05. Print Name / Project PrintMyName

---
+++ CSharp-Part-1/Intro-Programming-Homework/05. Print Name/PrintMyName.cs
@@ -0,0 +1,12 @@
+﻿namespace _05.Print_Name
+{
+    using System;
+
+    public class PrintMyName
+    {
+        public static void Main()
+        {
+            Console.WriteLine("beBoss");
+        }
+    }
+}
