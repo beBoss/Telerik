a02ebc52ee82a011a9a396d809d175aa8288998f
beBoss™ <beBoss.bg@gmail.com>
2016-04-02 17:32:07 +0100

    Added problem 10. Format Code

---
+++ CSharp-Part-1/Intro-Programming-Homework/10. Format Code/HorribleCode.cs
@@ -0,0 +1,18 @@
+﻿namespace _10.Format_Code
+{
+    using System;
+
+    public class HorribleCode
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hi, I am horribly formatted program");
+            Console.WriteLine("Numbers and squares:");
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i + " --> " + i * i);
+            }
+        }
+    }
+}
