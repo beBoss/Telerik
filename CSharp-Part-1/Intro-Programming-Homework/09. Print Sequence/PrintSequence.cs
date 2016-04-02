9faa6cdab73ec566c3d9642b46109b3d851ca8dc
beBoss™ <beBoss.bg@gmail.com>
2016-04-02 17:24:52 +0100

    Added problem - Print Sequence

---
+++ CSharp-Part-1/Intro-Programming-Homework/09. Print Sequence/PrintSequence.cs
@@ -0,0 +1,15 @@
+﻿namespace _09.Print_Sequence
+{
+    using System;
+
+    public class PrintSequence
+    {
+        public static void Main()
+        {
+            for (var i = 2; i < 12; i++)
+            {
+                Console.WriteLine("{0}", i % 2 == 0 ? i  : i * (-1));
+            }
+        }
+    }
+}
