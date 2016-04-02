4a087de39762b8d23fa41d71d9dc88f75f13ccff
beBoss™ <beBoss.bg@gmail.com>
2016-04-02 18:15:26 +0100

    Added Problem 14 Current date and time

---
+++ CSharp-Part-1/Intro-Programming-Homework/14. Current Date and Time/PrintCurrentDateAndTime.cs
@@ -0,0 +1,12 @@
+﻿namespace _14.Current_Date_and_Time
+{
+    using System;
+
+    public class PrintCurrentDateAndTime
+    {
+        public static void Main()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
