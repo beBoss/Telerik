c94432ed10067a6ed4cf669142e9fb2fd3e6f959
beBoss™ <beBoss.bg@gmail.com>
2016-04-03 02:26:46 +0100

    Added 02. Data-Types-and-Variables homework Added Problem 01.Declare_Variables

---
+++ CSharp-Part-1/02. Data-Types-and-Variables/01. Declare Variables/DeclareVariables.cs
@@ -0,0 +1,19 @@
+﻿namespace _01.Declare_Variables
+{
+    using System;
+
+    public class DeclareVariables
+    {
+        public static void Main()
+        {
+            ushort numOne = 52130;
+            sbyte numTwo = -115;
+            int numThree = 4825932;
+            byte numFour = 97;
+            short numFive = -10000;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}, {4}", 
+                numOne, numTwo, numThree, numFour, numFive);
+        }
+    }
+}
