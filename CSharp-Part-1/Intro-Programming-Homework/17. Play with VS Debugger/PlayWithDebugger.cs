5d00d0ba050070a1c43880b3e7d988dad89e5cec
beBoss™ <beBoss.bg@gmail.com>
2016-04-03 02:09:23 +0100

    Added _17.Play_with_VS_Debugger

---
+++ CSharp-Part-1/Intro-Programming-Homework/17. Play with VS Debugger/PlayWithDebugger.cs
@@ -0,0 +1,15 @@
+﻿namespace _17.Play_with_VS_Debugger
+{
+    using System;
+
+    public class PlayWithDebugger
+    {
+        public static void Main()
+        {
+            for (var i = 1; i <= 1000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
