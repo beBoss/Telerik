a3c354fb2ded8de95d5de4d0314fb2530c08dd7e
beBoss™ <beBoss.bg@gmail.com>
2016-04-03 01:55:49 +0100

    Added problem 15 - Age

---
+++ CSharp-Part-1/Intro-Programming-Homework/15. Age/CalculateAge.cs
@@ -0,0 +1,25 @@
+﻿namespace _15.Age
+{
+    using System;
+    using System.Globalization;
+
+    public class CalculateAge
+    {
+        public static void Main()
+        {
+            var birthDate = Console.ReadLine();
+            var pattern = "MM.dd.yyyy";
+
+            var parsedDate = DateTime.ParseExact(birthDate, pattern, CultureInfo.InvariantCulture);
+
+            var today = DateTime.Now;
+            var age = today.Year - parsedDate.Year;
+
+            if (today < parsedDate.AddYears(age)) age--;
+
+            Console.WriteLine(age);
+            Console.WriteLine(age + 10);
+
+        }
+    }
+}
