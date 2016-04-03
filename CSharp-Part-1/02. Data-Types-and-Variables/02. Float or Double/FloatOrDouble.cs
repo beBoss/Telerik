39581d3f1663018e156b4195cb478d59d1a8d191
beBoss™ <beBoss.bg@gmail.com>
2016-04-03 02:37:45 +0100

    Added 02.Float_or_Double

---
+++ CSharp-Part-1/02. Data-Types-and-Variables/02. Float or Double/FloatOrDouble.cs
@@ -0,0 +1,18 @@
+﻿namespace _02.Float_or_Double
+{
+    using System;
+
+    public class FloatOrDouble
+    {
+        public static void Main()
+        {
+            double numOne = 34.567839023; 
+            float numTwo = 12.345F;
+            double numThree = 8923.1234857; 
+            float numFour = 3456.091F;
+
+            Console.WriteLine("{0}, {1}, {2}, {3}", 
+                numOne, numTwo, numThree, numFour);
+        }
+    }
+}
