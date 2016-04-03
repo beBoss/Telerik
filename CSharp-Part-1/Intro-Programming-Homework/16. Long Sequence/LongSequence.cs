95c13e7d198762bd2d9d0fe710922d49a3ffcf3a
beBoss™ <beBoss.bg@gmail.com>
2016-04-03 02:04:51 +0100

    Added Problem 16.Long_Sequence

---
+++ CSharp-Part-1/Intro-Programming-Homework/16. Long Sequence/LongSequence.cs
@@ -0,0 +1,15 @@
+﻿namespace _16.Long_Sequence
+{
+    using System;
+
+    public class LongSequence
+    {
+        public static void Main()
+        {
+            for (var i = 2; i < 1002; i++)
+            {
+                Console.WriteLine("{0}", i % 2 == 0 ? i : -i);
+            }
+        }
+    }
+}
